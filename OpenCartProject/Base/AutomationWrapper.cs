a04a7dfd158d515c162946eabf6cfe2a32650689
komalj <jadhavkk39@gmail.com>
2023-01-10 15:15:58 +0530

    Add project files.

---
+++ OpenCartProject/Base/AutomationWrapper.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartProject.Base
+{
+    public class AutomationWrapper
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void BeforeMethod()
+        {
+            string browserName = "";
+            if (browserName.ToLower().Equals("edge"))
+            {
+                driver = new EdgeDriver();
+            }
+            else if (browserName.ToLower().Equals("firefox"))
+            {
+                driver = new FirefoxDriver();
+            }
+            else
+            {
+                driver = new ChromeDriver();
+            }
+
+
+
+            //   driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "https://www.opencart.com/";
+
+
+        }
+
+        [TearDown]
+        public void AfterMethod()
+        {
+
+            driver.Quit();
+        }
+
+
+    }
+
+}
+
---
+++ OpenCartProject/Utilities/DataSource.cs
@@ -0,0 +1,122 @@
+﻿using OpenCartProject.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartProject.Utilities
+{
+    public class DataSource : AutomationWrapper
+    {
+
+        public static object[] InvalidRegisterData()
+
+        {
+
+            string[] dataSet1 = new string[6];
+            dataSet1[0] = "sakshi devkar";
+            dataSet1[1] = "shakti";
+            dataSet1[2] = "jack";
+            dataSet1[3] = "sdevkar39@gmail.com";
+            dataSet1[4] = "India";
+            dataSet1[5] = "devkar@00";
+
+
+
+            object[] allDataSet = new object[1];
+            allDataSet[0] = dataSet1;
+
+
+            return allDataSet;
+
+
+        } 
+
+
+
+
+
+        public static object[] ValidRegisterData()
+
+        {
+
+            string[] dataSet1 = new string[6];
+            dataSet1[0] = "sakshi devkar";
+            dataSet1[1] = "sakshi";
+            dataSet1[2] = "devkar";
+            dataSet1[3] = "sdevkar39@gmail.com";
+            dataSet1[4] = "India";
+            dataSet1[5] = "devkar@00";
+
+           
+            object[] allDataSet = new object[1]; 
+            allDataSet[0] = dataSet1;
+          
+
+            return allDataSet;
+
+
+        }
+
+
+
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*public static object[] InvalidLoginData2()
+        {
+            object[] data = ExcelUtils.GetSheetIntoObjectArray
+                (@"", "InvalidRegisterTest");
+            return data;
+        }
+
+
+        public static object[] AddUserTest()
+        {
+            object[] data = ExcelUtils.GetSheetIntoObjectArray
+                (@"", "AddUserTest");
+            return data;
+        }*/
+    }
+}
+
+    
+
